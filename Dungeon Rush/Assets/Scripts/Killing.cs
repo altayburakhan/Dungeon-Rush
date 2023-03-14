3c991c08d36f51fde9ebdbb6f716427f5fd2c73b
Burakhan Altay <burakhan.altay@gmail.com>
2023-03-14 17:47:00 +0300

    Killing Script

---
+++ Dungeon Rush/Assets/Scripts/Killing.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Killing : GameManager
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            isDead = true;
+        }
+    }
+}
